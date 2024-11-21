ac5b9ac49e0a840037801fffb77ccba6a6aef1b1
舒小凯 <2581105856@qq.com>
2024-11-21 22:24:52 +0800

    upd: 增强变量管理功能，支持多类型变量

--- src/renderer/pages/modules/apidoc/doc-edit/content/variable/variable.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/variable/variable.vue
@@ -1,21 +1,83 @@
-
 <template>
   <div class="s-variable">
     <!-- 新增变量 -->
     <SFieldset :title="t('新增变量')" class="left">
       <el-form ref="form" :model="formInfo" :rules="rules" label-width="120px">
         <el-form-item :label="`${t('变量名称')}：`" prop="name">
-          <el-input v-model="formInfo.name" :size="config.renderConfig.layout.size" :placeholder="t('请输入变量名称')" class="w-100" maxlength="100" clearable></el-input>
-        </el-form-item>
-        <el-form-item :label="`${t('变量值')}：`" prop="value">
-          <el-input v-model="formInfo.value" type="textarea" :autosize="{ minRows: 10, maxRows: 10 }" :size="config.renderConfig.layout.size" :placeholder="t('请输入变量值')" class="w-100" maxlength="9999" clearable></el-input>
+          <el-input v-model="formInfo.name" :size="config.renderConfig.layout.size" :placeholder="t('请输入变量名称')"
+            class="w-100" show-word-limit maxlength="100" clearable>
+          </el-input>
         </el-form-item>
         <el-form-item :label="`${t('值类型')}：`" prop="type">
           <el-select v-model="formInfo.type" :size="config.renderConfig.layout.size" class="w-100">
             <el-option value="string" label="string"></el-option>
             <el-option value="number" label="number"></el-option>
             <el-option value="boolean" label="boolean"></el-option>
+            <el-option value="null" label="null"></el-option>
+            <el-option value="file" label="file"></el-option>
+            <el-option value="any" label="any"></el-option>
           </el-select>
+        </el-form-item>
+        <!-- 字符串 -->
+        <el-form-item v-if="formInfo.type === 'string'" :label="`${t('变量值')}：`" prop="value">
+          <el-input v-model="formInfo.stringValue" type="textarea" :autosize="{ minRows: 10, maxRows: 10 }"
+            :size="config.renderConfig.layout.size" show-word-limit :placeholder="t('请输入任意字符')" class="w-100"
+            maxlength="9999" clearable>
+          </el-input>
+        </el-form-item>
+        <!-- 数字 -->
+        <el-form-item v-if="formInfo.type === 'number'" :label="`${t('变量值')}：`" prop="value">
+          <el-input-number v-model="formInfo.numberValue" :size="config.renderConfig.layout.size"
+            :placeholder="t('请输入任意数字')" class="w-100" :controls="false">
+          </el-input-number>
+        </el-form-item>
+        <!-- 布尔值 -->
+        <el-form-item v-if="formInfo.type === 'boolean'" :label="`${t('变量值')}：`" prop="value">
+          <el-radio v-model="formInfo.booleanValue" :label="true">ture</el-radio>
+          <el-radio v-model="formInfo.booleanValue" :label="false">false</el-radio>
+        </el-form-item>
+        <!-- 文件值 -->
+        <el-form-item v-if="formInfo.type === 'file'" :label="`${t('变量值')}：`" prop="value">
+          <el-upload ref="upload" class="upload-demo" @change="handleSelectFile" @exceed="handleExceed" :limit="1"
+            :auto-upload="false" :show-file-list="false">
+            <template #trigger>
+              <el-button size='small'>选择文件</el-button>
+            </template>
+            <template #default>
+              <div v-if="formInfo.fileValue.name">
+                <div class="d-flex a-center">
+                  <span class="flex0 theme-color">文件名称：</span>
+                  <span :title="formInfo.fileValue.name" class="text-ellipsis">{{ formInfo.fileValue.name }}</span>
+                </div>
+                <div class="d-flex a-center">
+                  <span class="flex0 theme-color">文件路径：</span>
+                  <span :title="formInfo.fileValue.path" class="text-ellipsis">{{ formInfo.fileValue.path }}</span>
+                </div>
+                <div class="d-flex a-center">
+                  <span class="flex0 theme-color">mime类型：</span>
+                  <span :title="formInfo.fileValue.fileType" class="text-ellipsis">{{ formInfo.fileValue.fileType }}</span>
+                </div>
+              </div>
+            </template>
+          </el-upload>
+        </el-form-item>
+        <!-- 任意类型 -->
+        <el-form-item v-if="formInfo.type === 'any'" :label="`${t('变量值')}：`" prop="value">
+          <div class="h-300px w-100 border-gray-400">
+            <SJsonEditor 
+              v-model="formInfo.anyValue"
+              ref="jsonComponent" 
+              :config="{
+                language: 'javascript',
+                lineNumbers: 'off',
+                placeholder: `eg: \nMath.random() \n new Date()  \n'i am a string' \n123 \ntrue \nfalse \nnull \n{a: 1} \n[a, 1, 2, 3] \n(() => { 
+                const num = 3;
+                return Math.random() + num 
+                })()`,
+              }"
+            >
+            </SJsonEditor>
+          </div>
         </el-form-item>
         <div class="d-flex j-end">
           <el-button :loading="loading" type="primary" @click="handleAddVariable">{{ t("确认添加") }}</el-button>
@@ -24,32 +86,44 @@
     </SFieldset>
     <!-- 变量列表 -->
     <SFieldset :title="t('变量列表')" class="right">
-      <STable
-        ref="table"
-        url="/api/project/project_variable"
-        delete-many
-        delete-url="/api/project/project_variable"
-        :delete-params="{ projectId: route.query.id }"
-        :params="{ projectId: route.query.id }"
-      >
+      <STable ref="table" url="/api/project/project_variable" delete-many delete-url="/api/project/project_variable"
+        :delete-params="{ projectId: route.query.id }" :params="{ projectId: route.query.id }">
         <el-table-column :label="t('变量名称')" align="center">
           <template #default="scope">
-            <el-input v-if="scope.row.__active" v-model="scope.row.name" :size="config.renderConfig.layout.size" class="w-100" maxlength="100" clearable></el-input>
+            <el-input v-if="scope.row.__active" v-model="scope.row.name" :size="config.renderConfig.layout.size"
+              class="w-100" maxlength="100" clearable></el-input>
             <span v-else>{{ scope.row.name }}</span>
           </template>
         </el-table-column>
         <el-table-column :label="t('变量值')" align="center" show-overflow-tooltip>
           <template #default="scope">
-            <el-input v-if="scope.row.__active" v-model="scope.row.value" type="textarea" :autosize="{ minRows: 2, maxRows: 10 }" :size="config.renderConfig.layout.size" class="w-100" maxlength="9999" clearable></el-input>
+            <div v-if="scope.row.type === 'file'">
+              <div>
+                <span>文件名称：</span>
+                <span>{{ scope.row.fileValue.name }}</span>
+              </div>
+              <div>
+                <span>文件路径：</span>
+                <span>{{ scope.row.fileValue.path }}</span>
+              </div>
+              <div>
+                <span>mime类型：</span>
+                <span>{{ scope.row.fileValue.fileType }}</span>
+              </div>
+            </div>
             <span v-else>{{ scope.row.value }}</span>
           </template>
         </el-table-column>
         <el-table-column :label="t('变量类型')" align="center">
           <template #default="scope">
-            <el-select v-if="scope.row.__active" v-model="scope.row.type" :size="config.renderConfig.layout.size" class="w-100">
+            <el-select v-if="scope.row.__active" v-model="scope.row.type" :size="config.renderConfig.layout.size"
+              class="w-100">
               <el-option value="string" label="string"></el-option>
               <el-option value="number" label="number"></el-option>
               <el-option value="boolean" label="boolean"></el-option>
+              <el-option value="null" label="null"></el-option>
+              <el-option value="any" label="any"></el-option>
+              <el-option value="file" label="file"></el-option>
             </el-select>
             <span v-else>{{ scope.row.type }}</span>
           </template>
@@ -57,14 +131,13 @@
         <el-table-column :label="t('创建者')" align="center" prop="creator"></el-table-column>
         <el-table-column :label="t('操作')" align="center">
           <template #default="scope">
-            <el-button v-show="!scope.row.__active && !isEditing" link type="primary" text @click="handleEdit(scope.row)">{{ t("编辑") }}</el-button>
-            <el-button v-show="scope.row.__active" link type="primary" text @click="handleSubmitEdit(scope.row)">{{ t("确认") }}</el-button>
-            <el-button v-show="scope.row.__active" link type="primary" text @click="handleCancelEdit(scope.row)">{{ t("取消") }}</el-button>
+            <el-button link type="primary" text @click="handleEdit(scope.row)">{{ t("编辑") }}</el-button>
             <el-button link type="primary" text @click="handleDelete(scope.row._id)">{{ t("删除") }}</el-button>
           </template>
         </el-table-column>
       </STable>
     </SFieldset>
+    <EditDialog v-model="isShowEditDialog" :editData="oldEditingData" @success="getData" @close="handleCloseEditDialog"></EditDialog>
   </div>
 </template>
 
@@ -73,43 +146,98 @@ import { useApidocBaseInfo } from '@/store/apidoc/base-info';
 import { ApidocProjectVariable } from '@src/types/apidoc/base-info';
 import SFieldset from '@/components/common/fieldset/g-fieldset.vue'
 import STable from '@/components/common/table/g-table.vue'
-import type { ApidocVariable } from '@src/types/global'
 import { config } from '@src/config/config'
 import { t } from 'i18next'
 import { ref } from 'vue';
-import { ElMessage, ElMessageBox, FormInstance } from 'element-plus';
+import { ElMessage, ElMessageBox, FormInstance, genFileId, UploadFile, UploadInstance, UploadProps, UploadRawFile } from 'element-plus';
 import { request } from '@/api/api';
 import { useRoute } from 'vue-router';
+import SJsonEditor from '@/components/common/json-editor/g-json-editor.vue'
+import EditDialog from './dialog/edit.vue'
 
-type EditApidocVariable = ApidocVariable & {
-  __active?: boolean,
+
+
+export type AddProjectVariableParams = {
+  _id?: string;
+  projectId: string,
+  name: string,
+  value: string,
+  type: 'string' | 'number' | 'boolean' | 'null' | 'any' | 'file',
+  fileValue: {
+    name: string,
+    path: string,
+    fileType: string,
+  }
 }
-
-const formInfo = ref({
-  name: '', //------------变量名称
-  value: '', //-----------变量值
-  type: 'string', //------变量类型
+export type AddProjectVariableFormInfo = {
+  name: string,
+  stringValue: string,
+  numberValue: number,
+  booleanValue: boolean,
+  nullValue: null,
+  anyValue: string,
+  fileValue: {
+    name: string,
+    path: string,
+    fileType: string,
+  },
+  type: 'string' | 'number' | 'boolean' | 'null' | 'any' | 'file',
+}
+const formInfo = ref<AddProjectVariableFormInfo>({
+  name: '',
+  stringValue: '',
+  numberValue: 0,
+  booleanValue: true,
+  nullValue: null,
+  anyValue: '',
+  fileValue: {
+    name: "",
+    path: '',
+    fileType: '',
+  },
+  type: 'string',
 })
-const oldEditingData = ref<EditApidocVariable | null>(null);
 const rules = ref({
   name: [{ required: true, message: t('请输入变量名称'), trigger: 'blur' }],
-  value: [{ required: true, message: t('请输入变量值'), trigger: 'blur' }],
 })
-const isEditing = ref(false);
+const oldEditingData = ref<AddProjectVariableParams | null>(null);
+const isShowEditDialog = ref(false);
 const loading = ref(false);
 const route = useRoute()
-const table = ref<{ getData: () => Promise<{
-  data: {
-    rows: ApidocProjectVariable[]
-  }
-}> }>();
+const table = ref<{
+  getData: () => Promise<{
+    data: {
+      rows: ApidocProjectVariable[]
+    }
+  }>
+}>();
 const form = ref<FormInstance>();
 const apidocBaseInfoStore = useApidocBaseInfo();
+const upload = ref<UploadInstance>()
 /*
 |--------------------------------------------------------------------------
 | 方法定义
 |--------------------------------------------------------------------------
 */
+const handleSelectFile = (file: UploadFile) => {
+  formInfo.value.fileValue = {
+    name: file.name,
+    path: file.raw!.path,
+    fileType: file.raw!.type,
+  }
+  return false;
+}
+const handleExceed: UploadProps['onExceed'] = (files) => {
+  upload.value!.clearFiles();
+  const file = files[0] as UploadRawFile
+  file.uid = genFileId()
+  upload.value!.handleStart(file)
+  formInfo.value.fileValue = {
+    name: file.name,
+    path: file.path,
+    fileType: file.type,
+  }
+}
 const getData = () => {
   table.value?.getData().then((res) => {
     apidocBaseInfoStore.changeVariables(res.data.rows)
@@ -120,9 +248,30 @@ const handleAddVariable = () => {
   form.value?.validate((valid) => {
     if (valid) {
       loading.value = true;
-      const params = Object.assign(formInfo.value, {
-        projectId: route.query.id,
-      });
+      const params: AddProjectVariableParams =  {
+        projectId: route.query.id as string,
+        name: formInfo.value.name,
+        type: formInfo.value.type,
+        value: "",
+        fileValue: {
+          name: '',
+          path: '',
+          fileType: '',
+        }
+      };
+      if (formInfo.value.type === 'string') {
+        params.value = formInfo.value.stringValue;
+      } else if (formInfo.value.type === 'number') {
+        params.value = formInfo.value.numberValue.toString();
+      } else if (formInfo.value.type === 'boolean') {
+        params.value = formInfo.value.booleanValue.toString();
+      } else if (formInfo.value.type === 'null') {
+        params.value = 'null';
+      } else if (formInfo.value.type === 'any') {
+        params.value = formInfo.value.anyValue;
+      } else if (formInfo.value.type === 'file') {
+        params.fileValue = formInfo.value.fileValue;
+      }
       request.post('/api/project/project_variable', params).then(() => {
         getData();
       }).catch((err) => {
@@ -133,32 +282,13 @@ const handleAddVariable = () => {
     }
   });
 }
+const handleCloseEditDialog = () => {
+  isShowEditDialog.value = false;
+}
 //让当前行处于修改状态
-const handleEdit = (row: EditApidocVariable) => {
-  row.__active = true;
-  isEditing.value = true;
+const handleEdit = (row: AddProjectVariableParams) => {
+  isShowEditDialog.value = true;
   oldEditingData.value = JSON.parse(JSON.stringify(row));
-}
-//确认修改当前行
-const handleSubmitEdit = (row: EditApidocVariable) => {
-  row.__active = false;
-  const params = {
-    ...row,
-    projectId: route.query.id,
-  };
-  request.put('/api/project/project_variable', params).then(() => {
-    ElMessage.success(t('修改成功'));
-    getData();
-    isEditing.value = false;
-  }).catch((err) => {
-    console.error(err);
-  });
-}
-//取消修改
-const handleCancelEdit = (row: EditApidocVariable) => {
-  Object.assign(row, oldEditingData)
-  row.__active = false;
-  isEditing.value = false;
 }
 //=====================================删除====================================//
 //删除一个数据
@@ -184,17 +314,19 @@ const handleDelete = (_id: string) => {
 
 <style lang="scss" scoped>
 .s-variable {
-    overflow-y: auto;
-    height: calc(100vh - #{size(100)});
-    width: 100%;
-    padding: size(20) size(30);
-    display: flex;
-    .left {
-        flex: 0 0 size(400);
-        margin-right: size(10);
-    }
-    .right {
-        flex: 1;
-    }
+  overflow-y: auto;
+  height: calc(100vh - #{size(100)});
+  width: 100%;
+  padding: size(20) size(30);
+  display: flex;
+
+  .left {
+    flex: 0 0 size(500);
+    margin-right: size(10);
+  }
+
+  .right {
+    flex: 1;
+  }
 }
 </style>
