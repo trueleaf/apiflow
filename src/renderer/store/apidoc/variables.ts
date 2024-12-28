7b24872c9f1883fc0491b90e21af40fb03cacb70
舒小凯 <2581105856@qq.com>
2024-12-28 13:33:47 +0800

    feat: 全局变量新增对象类型

--- src/renderer/store/apidoc/variables.ts
+++ src/renderer/store/apidoc/variables.ts
@@ -1,10 +1,11 @@
+import { getObjectVariable } from "@/utils/utils";
 import { ApidocVariable } from "@src/types/global";
 import { defineStore } from "pinia"
 import { ref } from 'vue';
 
 export const useVariable = defineStore('apidocVariable', () => {
   const variables = ref<ApidocVariable[]>([]);
-
+  const objectVariable = ref<Record<string, any>>({})
   //改变变量值
   const changeVariableById = (id: string, varInfo: ApidocVariable) => {
     variables.value.forEach((item) => {
@@ -12,13 +13,20 @@ export const useVariable = defineStore('apidocVariable', () => {
         Object.assign(item, varInfo)
       }
     })
+    getObjectVariable(variables.value).then((value) => {
+      objectVariable.value = value;
+    })
   }
   //替换所有变量
   const replaceVariables = (varList: ApidocVariable[]) => {
-    variables.value.splice(0, varList.length, ...varList)
+    variables.value.splice(0, varList.length, ...varList);
+    getObjectVariable(variables.value).then((value) => {
+      objectVariable.value = value;
+    })
   }
   return {
     variables,
+    objectVariable,
     changeVariableById,
     replaceVariables,
   }
