c10c7284177760234053ad6f9eee7d32fca00ba2
舒小凯 <2581105856@qq.com>
2025-01-19 22:27:39 +0800

    enhance: 优化响应信息样式及宽度调整

--- src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/res-info/res-info.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/res-info/res-info.vue
@@ -1,17 +1,22 @@
-
 <template>
   <div class="d-flex a-center px-3 w-100 overflow-hidden">
     <div class="flex0 d-flex a-center">
       <span>{{ t("状态码") }}：</span>
       <template v-if="responseInfo.statusCode">
-        <span v-show="responseInfo.statusCode >= 100 && responseInfo.statusCode < 300" class="green">{{ responseInfo.statusCode }}</span>
-        <span v-show="responseInfo.statusCode >= 300 && responseInfo.statusCode < 400" class="orange">{{ responseInfo.statusCode }}</span>
+        <span v-show="responseInfo.statusCode >= 100 && responseInfo.statusCode < 300" class="green">{{
+          responseInfo.statusCode }}</span>
+        <span v-show="responseInfo.statusCode >= 300 && responseInfo.statusCode < 400" class="orange">{{
+          responseInfo.statusCode }}</span>
         <span v-show="responseInfo.statusCode >= 400" class="red">{{ responseInfo.statusCode }}</span>
       </template>
       <el-icon v-else :title="t('未请求数据')" :size="16" class="gray-500">
         <QuestionFilled />
       </el-icon>
     </div>
+    <!-- <div v-if="responseInfo.contentType" :title="responseInfo.contentType" class="content-type-wrap">
+      <div>测试环境</div>
+      <div>{{ responseInfo.contentType }}{{ responseInfo.contentType }}{{ responseInfo.contentType }}</div>
+    </div> -->
     <el-divider direction="vertical"></el-divider>
     <div class="flex0 d-flex a-center">
       <span>{{ t("时长") }}：</span>
@@ -37,9 +42,10 @@
       </el-icon>
     </div>
     <el-divider direction="vertical"></el-divider>
-    <div class="flex0 d-flex a-center">
-      <span>{{ t("格式") }}：</span>
-      <SEllipsisContent v-if="responseInfo.contentType" :value="responseInfo.contentType" :title="responseInfo.contentType" max-width="300px" class="orange"></SEllipsisContent>
+    <div class="content-type-wrap d-flex a-center">
+      <span class="flex0">{{ t("格式") }}：</span>
+      <div v-if="responseInfo.contentType" :title="responseInfo.contentType" class="content-type">{{
+        responseInfo.contentType }}{{ responseInfo.contentType }}{{ responseInfo.contentType }}</div>
       <el-icon v-else :title="t('未请求数据')" :size="16" class="gray-500">
         <QuestionFilled />
       </el-icon>
@@ -64,3 +70,17 @@ const formatedBytes = computed(() => formatBytes(responseInfo.value.bodyByteLeng
 const formatedMs = computed(() => formatMs(responseInfo.value.rt))
 </script>
 
+<style lang="scss" scoped>
+.content-type-wrap {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  .content-type {
+    color: $orange;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+</style>
