68f8ded55a25f1a94d389363710e8d89407f5fa6
舒小凯 <2581105856@qq.com>
2025-02-25 12:57:17 +0800

    upd: 优化全局快捷键绑定逻辑

--- src/renderer/shortcut/index.ts
+++ src/renderer/shortcut/index.ts
@@ -3,11 +3,7 @@
 
 export const bindGlobalShortCut = () => {
   const bind = (e: KeyboardEvent) => {
-    if (e.ctrlKey && e.shiftKey && (e.key === 'r' || e.key === 'R')) { //强制刷新条件需要前置，防止ctrl+r先执行
-      window.electronAPI?.reloadIgnoringCache()
-    } else if (e.ctrlKey && (e.key === 'r' || e.key === 'R')) {
-      window.electronAPI?.reload()
-    } 
+   
   }
   document.documentElement.removeEventListener('keydown', bind)
   document.documentElement.addEventListener('keydown', bind)
