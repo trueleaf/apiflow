1802d2266e5db0c78c2e44b5d54bbda27d09fcde
wildsell <2581105856@qq.com>
2025-08-13 12:35:54 +0800

    feat: 添加虚拟滚动支持到SSE视图组件，以优化大数据流的渲染性能

--- packages/web/src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/body/body.vue
+++ packages/web/src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/body/body.vue
@@ -60,7 +60,7 @@
     <template v-if="apidocResponseStore.responseInfo.contentType">
       <!-- eventStream -->
       <div v-if="apidocResponseStore.responseInfo.responseData.canApiflowParseType === 'textEventStream'" class="sse-view-wrap">
-        <SSseView :data-list="apidocResponseStore.responseInfo.responseData.streamData" />
+        <SSseView :data-list="apidocResponseStore.responseInfo.responseData.streamData" :virtual="apidocResponseStore.requestState === 'finish'"/>
       </div>
       <!-- 图片类型 -->
       <div v-if="apidocResponseStore.responseInfo.responseData.canApiflowParseType === 'image'" class="img-view-wrap">
