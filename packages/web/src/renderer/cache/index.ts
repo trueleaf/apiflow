c802d485dd20c53dbab4a82b42e571810bf95dc4
wildsell <2581105856@qq.com>
2025-10-22 19:21:27 +0800

    refactor: correct projectCache import path and remove unused component types

--- packages/web/src/renderer/cache/index.ts
+++ packages/web/src/renderer/cache/index.ts
@@ -1,6 +1,6 @@
 // 统一导出所有 cache 单例，方便使用
 export { apiNodesCache } from './standalone/apiNodesCache';
-export { projectCache } from './project/projectsCache';
+export { projectCache } from './project/projectCache';
 export { commonHeaderCache } from './project/commonHeadersCache';
 export { standaloneRuleCache } from './standalone/rulesCache';
 export { nodeVariableCache } from './variable/nodeVariableCache';
